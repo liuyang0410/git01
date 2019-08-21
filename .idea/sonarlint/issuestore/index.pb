b564927a8810fc532b9278bc2e86c8048c1ac24c
liuyang <bjj410@qq.com>
2019-08-21 13:46:47 +0800

    git 1.0

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+R
+"gitdemo/src/main/webapp/index.html,5\f\5fd1b0f69fae7e713b6911aa22f62e5681cb1885
+Y
+)gitdemo/src/main/java/com/admin/User.java,a\2\a2009c5522d2606ad2719acb97f963e256815498
---
+++ gitdemo/src/main/java/com/admin/User.java
@@ -0,0 +1,12 @@
+package com.admin;
+
+/**
+ * @description:
+ * @author: LiuYang
+ * @create: 23:34
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println(321);
+    }
+}
