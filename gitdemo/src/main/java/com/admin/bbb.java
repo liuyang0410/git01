2c419d945b4c8eea9c19ef40cb4271145e1def9f
liuyang <bjj410@qq.com>
2019-08-26 12:30:38 +0800

    git java分支第一次提交

---
+++ gitdemo/src/main/java/com/admin/bbb.java
@@ -0,0 +1,12 @@
+package com.admin;
+
+/**
+ * @description:
+ * @author: LiuYang
+ * @create: 12:29
+ */
+public class bbb {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
